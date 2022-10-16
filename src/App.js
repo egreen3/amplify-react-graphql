40c65b2ee8bb0e526ba1c42132add7f1c5484226
Elwood Green <elwood.green3@gmail.com>
2022-10-15 22:21:12 -0400

    2ND commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload - EGIII Edited.
+          Edit <code>src/App.js</code> and save to reload - Test is Live & UPDATED.
         </p>
         <a
           className="App-link"
